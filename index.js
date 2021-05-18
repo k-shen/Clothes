50d0b69831014d26211aa3ca42ccdf5871c7f646
Kaiwen Shen <kev.shen991206@gmail.com>
2021-05-18 17:52:34 -0500

    update

--- index.js
+++ index.js
@@ -21,6 +21,21 @@ var temp_feel;
 var sun_rise;
 var sun_set;
 
+class Clothes {
+    constructor(name, temp, id) {
+        this.name = name;
+        this.temp = temp;
+        this.id = id;
+    }
+
+    show() {
+        var doc = document.createElement('label');
+        var word = document.createTextNode('+ ' + this.name + ' ');
+        doc.appendChild(word);
+        document.getElementById(this.id).appendChild(doc);
+    }
+}
+
 var tops = [];
 var bottoms = [];
 tops.push(new Clothes('short T', 5, 'top'));
